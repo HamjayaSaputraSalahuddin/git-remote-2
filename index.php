dbc9ed43d6366812429eeb61d46e0670eb16125f
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2022-08-18 11:13:52 +0800

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae quidem inventore eum beatae laudantium, autem veniam? Omnis iure tempore, in, velit rerum officiis sit nulla aliquam ducimus odit dolore accusamus.</p>
 </body>
 </html>
